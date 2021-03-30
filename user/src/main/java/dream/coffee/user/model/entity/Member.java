e9c5fd8006e795e174234f022062dab982184c5c
accidentlywoo <accidentlywoo@gmail.com>
2021-03-30 19:03:48 +0900

    feat : Entity 설계, Entity 기능

---
+++ user/src/main/java/dream/coffee/user/model/entity/Member.java
@@ -0,0 +1,79 @@
+package dream.coffee.user.model.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String memberId;
+	private String password;
+	private String email;
+	private boolean useMarketing;
+	private boolean isCertification;
+	private boolean isDormant;
+
+	protected Member(){}
+
+	private Member(String memberId, String password, String email, boolean useMarketing, boolean isCertification) {
+		this.memberId = memberId;
+		this.password = password;
+		this.email = email;
+		this.useMarketing = useMarketing;
+		this.isCertification = isCertification;
+	}
+
+	/**
+	 * 멤버계정 생성 - 회원가입리
+	 *
+	 * @param memberId
+	 * @param password
+	 * @param email
+	 * @param useMarketing
+	 * @return
+	 */
+	public static Member createMember(String memberId, String password, String email, boolean useMarketing, boolean isCertification){
+		return new Member(memberId, password, email, useMarketing,isCertification);
+	}
+
+	/**
+	 * 멤버 휴면 처리
+	 *
+	 * @param isDormant
+	 * @return
+	 */
+	public Long changeDormantMember(boolean isDormant){
+		this.isDormant = isDormant;
+		return this.id;
+	}
+
+	/**
+	 * 멤버 인증 처리
+	 *
+	 * @param isCertification
+	 * @return
+	 */
+	public Long changeCertificateMember(boolean isCertification){
+		this.isCertification = isCertification;
+		return this.id;
+	}
+
+	/**
+	 * 멤버 마케팅 사용여부 처
+	 *
+	 * @param useMarketing
+	 * @return
+	 */
+	public Long changeMarketingMember(boolean useMarketing){
+		this.useMarketing = useMarketing;
+		return this.id;
+	}
+
+}
