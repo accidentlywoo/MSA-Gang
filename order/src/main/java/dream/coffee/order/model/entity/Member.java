8aba7de4d921ee8339121a3f3f4240c484bdd66a
accidentlywoo <accidentlywoo@gmail.com>
2021-03-31 11:01:37 +0900

    feat : Add Read-only Member Entity on Order Module

---
+++ order/src/main/java/dream/coffee/order/model/entity/Member.java
@@ -0,0 +1,27 @@
+package dream.coffee.order.model.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Read Only
+ */
+@Entity
+@Getter
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String memberId;
+	private String password;
+	private String email;
+	private boolean useMarketing;
+	private boolean isCertification;
+	private boolean isDormant;
+
+	protected Member(){}
+}
