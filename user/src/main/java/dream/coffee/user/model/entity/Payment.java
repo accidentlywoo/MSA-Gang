d8e663dcd4e25c91ef51dece02a385ddc9728cdf
accidentlywoo <accidentlywoo@gmail.com>
2021-03-31 17:06:45 +0900

    feat : create Payment Entity

---
+++ user/src/main/java/dream/coffee/user/model/entity/Payment.java
@@ -0,0 +1,26 @@
+package dream.coffee.user.model.entity;
+
+
+import dream.coffee.user.model.entity.baseEntity.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(mappedBy = "payment")
+	private Order order;
+
+	private String paymentMethod;
+	
+	private int amountOfPayment;
+
+	// 승인여부
+	private boolean isApproval;
+
+	@Embedded
+	private BaseEntity baseEntity;
+}
