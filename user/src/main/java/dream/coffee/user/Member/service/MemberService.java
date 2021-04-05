77de1eaf73cc06cb395e145e31339f1845e46350
accidentlywoo <accidentlywoo@gmail.com>
2021-04-05 12:21:17 +0900

    feat : Create Find Member Info Service

--- user/src/main/java/dream/coffee/user/Member/service/MemberService.java
+++ user/src/main/java/dream/coffee/user/Member/service/MemberService.java
@@ -1,6 +1,7 @@
 package dream.coffee.user.Member.service;
 
 import dream.coffee.user.Member.repository.MemberRepository;
+import dream.coffee.user.model.dto.MemberInfoDto;
 import dream.coffee.user.model.dto.SignUpReqDto;
 import dream.coffee.user.model.entity.Member;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +25,9 @@ public class MemberService {
 	@Transactional
 	public Long SignUp(SignUpReqDto signUpReqDto){
 		Optional<Member> byMemberId = memberRepository.findByMemberId(signUpReqDto.getId());
-		if (byMemberId.isPresent()){
-			// TODO :: Exception Handling
-			throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
-		}
+		// TODO :: Exception Handling
+		if (byMemberId.isPresent()) throw new IllegalArgumentException("이미 존재하는 아이디 입니다.");
+
 		Member newMember = Member.createMember(
 				signUpReqDto.getId(), signUpReqDto.getName(),
 				signUpReqDto.getPwd(), signUpReqDto.getEmail(),
@@ -35,5 +35,25 @@ public class MemberService {
 		);
 		memberRepository.save(newMember);
 		return newMember.getId();
+	}
+
+	/**
+	 * 회원 아이디로 정보 조회
+	 *
+	 * @param memberId
+	 * @return
+	 */
+	public MemberInfoDto aMemberInfo(String memberId){
+		Optional<Member> findMemberById = memberRepository.findByMemberId(memberId);
+		if (!findMemberById.isPresent()) throw new IllegalArgumentException("존재하지 않은 아이디 입니다.");
+
+		return MemberInfoDto.builder()
+				.id(findMemberById.get().getMemberId())
+				.name(findMemberById.get().getMemberName())
+				.email(findMemberById.get().getEmail())
+				.isUseMarketing(findMemberById.get().isUseMarketing())
+				.isCertifivation(findMemberById.get().isCertification())
+				.joinedDate(findMemberById.get().getBaseEntity().getCreatedTime())
+				.build();
 	}
 }
