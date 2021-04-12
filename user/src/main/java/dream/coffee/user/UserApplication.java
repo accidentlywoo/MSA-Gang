a26e118a91af0975cf69c473dfa3ebba290741a2
accidentlywoo <accidentlywoo@gmail.com>
2021-04-12 23:50:20 +0900

    feat : Eureka Server / Client Setting & Random Port Setting Test

--- user/src/main/java/dream/coffee/user/UserApplication.java
+++ user/src/main/java/dream/coffee/user/UserApplication.java
@@ -2,10 +2,10 @@ package dream.coffee.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class UserApplication {
 
 	public static void main(String[] args) {
