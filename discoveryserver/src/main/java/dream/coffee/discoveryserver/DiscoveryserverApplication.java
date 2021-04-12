b8962ea894838bd2f1c5bf808fa80965074e3e80
accidentlywoo <accidentlywoo@gmail.com>
2021-04-12 22:57:47 +0900

    feat : Add Eureka Server Module

---
+++ discoveryserver/src/main/java/dream/coffee/discoveryserver/DiscoveryserverApplication.java
@@ -0,0 +1,13 @@
+package dream.coffee.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserverApplication.class, args);
+	}
+
+}
