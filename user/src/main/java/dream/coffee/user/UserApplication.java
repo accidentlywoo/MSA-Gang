21ef8a7d7e997999f072d3fa31261edb05e8b4c4
accidentlywoo <accidentlywoo@gmail.com>
2021-04-12 22:11:08 +0900

    feat : Add Spring Cloud Eureka Dependency And Attach Config

--- user/src/main/java/dream/coffee/user/UserApplication.java
+++ user/src/main/java/dream/coffee/user/UserApplication.java
@@ -2,8 +2,10 @@ package dream.coffee.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UserApplication {
 
 	public static void main(String[] args) {
