5abcbd850df8bf6411e342cd25c2f935c145dfe7
accidentlywoo <accidentlywoo@gmail.com>
2021-04-13 23:30:58 +0900

    feat : Spring Cloud GateWay Setting

---
+++ gateway/src/test/java/dream/coffee/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dream.coffee.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
