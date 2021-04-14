557cdbfed92ae8f8116c7043e932910b2fef035b
accidentlywoo <accidentlywoo@gmail.com>
2021-04-14 10:48:39 +0900

    test : Create SigninService Class , Extract SignIn Unit Tests

---
+++ user/src/main/java/dream/coffee/user/api/Member/service/SigninService.java
@@ -0,0 +1,14 @@
+package dream.coffee.user.api.Member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Log4j
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class SigninService {
+
+}
