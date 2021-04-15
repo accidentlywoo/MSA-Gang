b0d03bdfa63e4ca531682aca1b4990e110893d3d
accidentlywoo <accidentlywoo@gmail.com>
2021-04-15 23:13:44 +0900

    feat : DML Insert Member for Test

---
+++ DataBase/DML-Insert.sql
@@ -0,0 +1,2 @@
+-- Test Infomation
+INSERT INTO DreamCoffee.member (created_admin_id, created_time, updated_admin_id, updated_time, email, is_certification, is_dormant, member_id, member_name, password, use_marketing) VALUES (1, '2021-04-14 11:23:40', 1, '2021-04-14 11:23:43', 'test@email.com', true, false, 'alreadyExist', 'test', 'alreadyExist', true);
