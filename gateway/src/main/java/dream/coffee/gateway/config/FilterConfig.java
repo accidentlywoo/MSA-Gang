4fa557f4c26b3982b8a46caef3cf58e8e926d1c7
accidentlywoo <accidentlywoo@gmail.com>
2021-04-15 23:16:08 +0900

    feat : Spring Cloud Gateway Filter Configuration Practice :: Setting Req/Res Header Each MSA Service

---
+++ gateway/src/main/java/dream/coffee/gateway/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package dream.coffee.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/user/**")
+						.filters(f -> f.addRequestHeader("user-request", "user-request-header")
+								.addResponseHeader("user-response", "user-response-header")
+						).uri("http://localhost:8080/")
+				)
+				.build();
+
+	}
+}
--- user/src/main/java/dream/coffee/user/api/controller/LivenessController.java
+++ user/src/main/java/dream/coffee/user/api/controller/LivenessController.java
@@ -1,7 +1,9 @@
 package dream.coffee.user.api.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ public class LivenessController {
 	@GetMapping("/live")
 	public String liveness(){
 		return "user service alive";
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("user-request") String header){
+		System.out.println(header);
+		return "Hello World In User Service";
 	}
 }
