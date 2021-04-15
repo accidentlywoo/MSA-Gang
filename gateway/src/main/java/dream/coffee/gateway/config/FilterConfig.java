6003da64b93356c2d08aaa69e36e505c48a3a8f5
accidentlywoo <accidentlywoo@gmail.com>
2021-04-15 23:51:34 +0900

    feat : Spring Cloud Gateway Filter Configuration Practice :: Setting Req/Res Header Each MSA Service At YML (Java Config Annotations Comment for Not Driven)

--- gateway/src/main/java/dream/coffee/gateway/config/FilterConfig.java
+++ gateway/src/main/java/dream/coffee/gateway/config/FilterConfig.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class FilterConfig {
-	@Bean
+//	@Bean
 	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
 		return builder.routes()
 				.route(r -> r.path("/user/**")
