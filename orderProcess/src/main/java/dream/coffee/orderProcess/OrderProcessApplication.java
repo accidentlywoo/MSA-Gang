cf67f528593c08cda280e286a6d5966cec7bd60f
accidentlywoo <accidentlywoo@gmail.com>
2021-04-20 17:31:58 +0900

    feat : [OrderProcess] Eureka Client 의존성 추가, 설정. 헬스체크용 Controller 추가

--- orderProcess/src/main/java/dream/coffee/orderProcess/OrderProcessApplication.java
+++ orderProcess/src/main/java/dream/coffee/orderProcess/OrderProcessApplication.java
@@ -2,8 +2,10 @@ package dream.coffee.orderProcess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderProcessApplication {
 
 	public static void main(String[] args) {
