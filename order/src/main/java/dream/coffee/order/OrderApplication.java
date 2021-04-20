9f243715dc7dd5cd4f6d431081f3e9213a6d0d12
accidentlywoo <accidentlywoo@gmail.com>
2021-04-20 10:43:50 +0900

    feat : [Order] Add Config for join Eureka Client. Add Controller for health check

--- order/src/main/java/dream/coffee/order/OrderApplication.java
+++ order/src/main/java/dream/coffee/order/OrderApplication.java
@@ -2,8 +2,10 @@ package dream.coffee.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- order/src/main/java/dream/coffee/order/api/controller/LivenessController.java
+++ order/src/main/java/dream/coffee/order/api/controller/LivenessController.java
@@ -1,14 +1,28 @@
 package dream.coffee.order.api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/order")
 public class LivenessController {
 	@GetMapping("/live")
 	public String liveness(){
 		return "order service alive";
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("order-request") String header){
+		log.info(header);
+		return "Hello World In User Service";
+	}
+
+	@GetMapping("/check")
+	public String check(){
+		return "Quak Quak!";
 	}
 }
