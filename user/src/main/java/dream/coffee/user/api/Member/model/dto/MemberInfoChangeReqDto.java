35119f026b99ef176a74239df1c4a87d10a8a2b1
accidentlywoo <accidentlywoo@gmail.com>
2021-04-22 23:30:20 +0900

    feat : [User] Enhancement DTO Validation

--- user/src/main/java/dream/coffee/user/api/Member/model/dto/MemberInfoChangeReqDto.java
+++ user/src/main/java/dream/coffee/user/api/Member/model/dto/MemberInfoChangeReqDto.java
@@ -3,14 +3,23 @@ package dream.coffee.user.api.Member.model.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
 public class MemberInfoChangeReqDto {
 	@NotNull
+	@Size(min = 5, max = 20, message = "id must be equal or grater than 5 characters and less than 16 characters")
 	private String id;
+
+	@Size(max = 20, message = "name must be less than 20 characters")
 	private String name;
+
+	@Size(min = 2, message = "email not be less than 2 characters")
+	@Email
 	private String email;
+
 	private Boolean isUseMarketing;
 }
