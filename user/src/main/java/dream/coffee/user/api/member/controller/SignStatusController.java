5ebc4c454c395f8ca5cdb35ab9a8e15b29d66e85
accidentlywoo <accidentlywoo@gmail.com>
2021-04-29 21:24:13 +0900

    feat : 회원가입 컨트롤러 response 상태코드 201 (create data) 세팅

--- user/src/main/java/dream/coffee/user/api/member/controller/SignStatusController.java
+++ user/src/main/java/dream/coffee/user/api/member/controller/SignStatusController.java
@@ -1,12 +1,14 @@
 package dream.coffee.user.api.member.controller;
 
-import dream.coffee.user.api.member.model.dto.InfoDto;
-import dream.coffee.user.api.member.model.dto.SignInReqDto;
-import dream.coffee.user.api.member.model.dto.SignUpReqDto;
+import dream.coffee.user.api.member.dto.InfoDto;
+import dream.coffee.user.api.member.dto.SignInReqDto;
+import dream.coffee.user.api.member.dto.SignUpReqDto;
 import dream.coffee.user.api.member.service.InfomationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,9 +21,16 @@ import javax.validation.Valid;
 public class SignStatusController {
 	private final InfomationService memberService;
 
+	/**
+	 * 회원가입 컨트롤러
+	 *  성공 : 201 status code (데이터 생성)
+	 *
+	 * @param reqDto
+	 * @return
+	 */
 	@PostMapping("/signUp")
-	public Long signUp(@Valid @RequestBody SignUpReqDto reqDto){
-		return memberService.SignUp(reqDto);
+	public ResponseEntity<Long> signUp(@Valid @RequestBody SignUpReqDto reqDto){
+		return new ResponseEntity<Long>(memberService.SignUp(reqDto), HttpStatus.CREATED);
 	}
 
 	@PostMapping("/signIn")
