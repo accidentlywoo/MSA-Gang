8e266c4c9c45b39553f99f921a7ad9bd52bbdbe8
accidentlywoo <accidentlywoo@gmail.com>
2021-05-02 00:22:13 +0900

    feat : base entity 상속구조로 변경
    @embedded 는 공통 필드 추출시에 사용하자 :: 좀더 공부하기

--- user/src/main/java/dream/coffee/user/api/model/entity/baseEntity/BaseEntity.java
+++ user/src/main/java/dream/coffee/user/api/model/entity/baseEntity/BaseEntity.java
@@ -3,11 +3,12 @@ package dream.coffee.user.api.model.entity.baseEntity;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Data
-@Embeddable
-public class BaseEntity {
+@MappedSuperclass
+public abstract class BaseEntity {
 	private Long createdAdminId = 1L;
 	private LocalDateTime createdTime = LocalDateTime.now();
 	private Long updatedAdminId = 1L;
