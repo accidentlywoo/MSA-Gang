546ef429846bd1e82ef81a8b4233fcc69de42066
accidentlywoo <accidentlywoo@gmail.com>
2021-05-02 11:24:46 +0900

    test : [Order] 상품 제고수량 빼기 GREEN / RED 테스트 케이스, 픽스처

--- order/src/test/java/dream/coffee/order/api/model/entity/ProductTest.java
+++ order/src/test/java/dream/coffee/order/api/model/entity/ProductTest.java
@@ -4,6 +4,8 @@ import dream.coffee.order.api.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,36 +26,76 @@ class ProductTest {
 	private ProductRepository productRepository;
 
 	@Test
-	@DisplayName("GREEN 상품 조회")
-	public void 상품_조회(){
-		// given
-		// when
-		// than
-	}
-
-	@Test
 	@DisplayName("GREEN 상품 제고 빼기")
 	public void 상품_제고수량_빼기(){
 	    // given
-	    // when
-	    // than
-	}
+		int subtactStock = 10;
 
-	@Test
-	@DisplayName("RED 상품 제고 빼기 실패")
-	public void 상품_제고수량_빼기_실패(){
-		// given
+		final Product product = 상품등록_픽스처();
+
+		final int beforeStock = product.getCurrentStock();
+
 		// when
+		productRepository.save(product.subtractStock(subtactStock));
+
+		final Optional<Product> after = productRepository.findProductByCode(product.getCode());
+
 		// than
+		assertThat(after).isNotEmpty();
+
+		assertThat(after.get())
+				.extracting("currentStock","code", "name", "price", "sale")
+				.contains(beforeStock - subtactStock, product.getCode(), product.getName(), product.getPrice(), product.isSale());
 	}
 
+	@ParameterizedTest
+	@ValueSource(ints = {1,2,3,4,10,20,100})
+	@DisplayName("RED 상품 제고 빼기 실패")
+	public void 상품_제고수량_빼기_실패(int overStock){
+		// given
+		final Product product = 상품등록_픽스처();
+
+		int subtactStock = product.getCurrentStock() + overStock;
+
+		assertThatThrownBy(() -> {
+			productRepository.save(product.subtractStock(subtactStock));
+		}).isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("주문 수량을 초과하였습니다. 남은 수량 : " + product.getCurrentStock());
+	}
+
+	private Product 상품등록_픽스처(){
+		// given
+		String name = "test";
+		int price = 9999;
+		int stock = 99;
+		boolean sale = true;
+
+		// when
+		final Product product = Product.createProduct(name, price, stock, sale);
+
+		String productCode = product.getCode();
+
+		productRepository.save(product);
+
+		// than
+		final Optional<Product> productByCode = productRepository.findProductByCode(productCode);
+
+		assertThat(productByCode).isNotEmpty();
+
+		assertThat(productByCode.get())
+				.extracting("name", "price", "currentStock", "sale")
+				.contains(name, price, stock, sale);
+
+		return productByCode.get();
+	}
+
+//	@Test
 	@Rollback(value = false)
-	@Test
 	@DisplayName("GREEN 상품 등록 GREEN")
 	public void 상품_등록_GREEN(){
 		// given
-		String name = "book";
-		int price = 10000;
+		String name = "earl-gray-tea";
+		int price = 6000;
 		int stock = 1000;
 		boolean sale = true;
 
