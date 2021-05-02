cd6673172bf1ef571455f563330436e5dd47840f
accidentlywoo <accidentlywoo@gmail.com>
2021-05-02 10:53:13 +0900

    feat : [Order] 상품 entity 상품 생성 메소드 추가

--- order/src/main/java/dream/coffee/order/api/model/entity/Product.java
+++ order/src/main/java/dream/coffee/order/api/model/entity/Product.java
@@ -4,6 +4,9 @@ import dream.coffee.order.api.model.entity.baseEntity.BaseEntity;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Random;
+import java.util.UUID;
 
 @Getter
 @Entity
@@ -29,7 +32,31 @@ public class Product extends BaseEntity {
 
 	protected Product(){}
 
-	public Product calculateStock(int orderNumberOfProduct){
+	private Product(String code, String name, int price, int stock, boolean sale) {
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.currentStock = stock;
+		this.sale = sale;
+	}
+
+	public static Product createProduct(String name, int price, int stock, boolean sale){
+		if (price < 10){
+			throw new IllegalArgumentException("상품가격의 최소단위는 10원입니다.");
+		}
+
+		if (stock < 0){
+			throw new IllegalArgumentException("상품수량은 0개 이상이여야 합니다.");
+		}
+
+		return new Product(generateProductCode(name), name, price, stock, sale);
+	}
+
+	private static String generateProductCode(String name){
+		return name+ LocalDate.now();
+	}
+
+	public Product subtractStock(int orderNumberOfProduct){
 		if(currentStock < orderNumberOfProduct) {
 			throw new IllegalArgumentException("주문 수량을 초과하였습니다.");
 		}
@@ -38,4 +65,5 @@ public class Product extends BaseEntity {
 
 		return this;
 	}
+
 }
